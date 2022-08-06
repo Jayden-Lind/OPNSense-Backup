60a4de0e506e0555b950d33c93767da2cbb4d55e
Jayden Lind <jaydenlind@gmail.com>
2022-08-06 11:32:10 +1000

    Cleanup

--- backup.py
+++ backup.py
@@ -9,10 +9,6 @@ else:
     api_key = sys.argv[2]
     api_secret = sys.argv[3]
 
-    headers = {
-
-    }
-
     backup = requests.get(url=f"https://{hostname}/api/backup/backup/download", verify=False, auth=(api_key, api_secret))
 
     with open(f"{hostname}-{datetime.datetime.now().strftime('%I_%p_%d_%m_%Y')}.xml", 'wb') as file:
