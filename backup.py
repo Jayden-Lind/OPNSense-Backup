7fffb49ef36b1d25ce781f82f78e377519f2d0b3
Jayden Lind <jaydenlind@gmail.com>
2022-08-06 11:05:05 +1000

    INIT
    clean

---
+++ backup.py
@@ -0,0 +1,19 @@
+import sys
+import requests
+import datetime
+
+if len(sys.argv) != 4:
+    sys.exit("Not enough args supplied")
+else:
+    hostname = sys.argv[1]
+    api_key = sys.argv[2]
+    api_secret = sys.argv[3]
+
+    headers = {
+
+    }
+
+    backup = requests.get(url=f"https://{hostname}/api/backup/backup/download", verify=False, auth=(api_key, api_secret))
+
+    with open(f"{hostname}-{datetime.datetime.now().strftime('%I_%p_%d_%m_%Y')}.xml", 'wb') as file:
+        file.write(backup.content)
